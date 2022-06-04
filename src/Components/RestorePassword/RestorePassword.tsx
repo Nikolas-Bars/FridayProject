91cad3928f38e25bd41fc515e3c8510cc2d67e2a
Nikolas-Bars <kasp2409@mail.ru>
2022-06-04 12:21:32 +0300

    added new svg preloader and catching of errors

--- src/Components/RestorePassword/RestorePassword.tsx
+++ src/Components/RestorePassword/RestorePassword.tsx
@@ -4,14 +4,18 @@ import SuperInputText from "../../Common/c1-SuperInputText/SuperInputText";
 import SuperButton from "../../Common/c2-SuperButton/SuperButton";
 import {Navigate, useNavigate, useParams} from "react-router-dom";
 import {useDispatch, useSelector} from "react-redux";
-import {SendInstructionsTC} from "../../Bll/reducers/password-reducer";
+import {SendInstructionsTC, setErrorAC} from "../../Bll/reducers/password-reducer";
 import {AppStoreType} from "../../Bll/store";
+import preloader from "../../Common/img/Preloader.gif";
+import Preloader from "../../Common/Preloader/Preloader";
 
 export const RestorePassword = () => {
 
     const dispatch = useDispatch()
 
     const sendSuccess = useSelector<AppStoreType, boolean>(state => state.password.sendSuccess)
+    const loadingStatus = useSelector<AppStoreType, boolean>(state => state.login.loadingStatus) // for preloader
+    const error = useSelector<AppStoreType, string>(state => state.password.error)
 
     const [value, setValue] = useState<string>('')
 
@@ -19,6 +23,7 @@ export const RestorePassword = () => {
 
     const onChangeHandler = (newValue: string) => {
         setValue(newValue)
+        dispatch(setErrorAC(''))
     }
 
     const message = `<div style="background-color: lime; padding: 15px">password recovery link: <a href='http://localhost:3000/RestorePassword/$token$'>link</a></div>`
@@ -32,6 +37,11 @@ export const RestorePassword = () => {
 
 
 
+    if (loadingStatus) {
+        return <Preloader />
+    }
+
+
     return (
         <div className={s.main}>
             {sendSuccess && <Navigate to={'/check-email'}/>}
@@ -39,7 +49,8 @@ export const RestorePassword = () => {
                 <div>It-incubator</div>
                 <h4>Forgot your password?</h4>
                 <SuperInputText type={'email'} onChangeText={onChangeHandler} placeholder={'Email'}
-                                className={s.input}/>
+                                className={s.input} error={error && 'Проверьте правильность введенных данных.'}/>
+
                 <div className={s.text}>Enter your email address and we will send you further instructions</div>
                 <SuperButton onClick={onClickHandler}>Send Instructions</SuperButton>
                 <div className={s.text}>Did you remember your password?</div>
